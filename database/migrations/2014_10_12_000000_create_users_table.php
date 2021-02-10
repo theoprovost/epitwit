09609368c8a314babdb845ba695c0e9aaaaa8cf1
theoprovost <theo.provost@epitech.eu>
2021-02-10 18:42:13 +0100

    Default register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -15,14 +16,28 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->date('dob');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::statement("
+            CREATE TRIGGER date_check BEFORE INSERT ON users
+            FOR EACH ROW
+                BEGIN
+                    IF NEW.dob >= (DATE_SUB(NOW(), INTERVAL 13 YEAR)) THEN
+                    SIGNAL SQLSTATE '45000'
+                        SET MESSAGE_TEXT = 'You must be older than 13 to register';
+                    END IF;
+                END;
+        ");
     }
+
+
 
     /**
      * Reverse the migrations.
