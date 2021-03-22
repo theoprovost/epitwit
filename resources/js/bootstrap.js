be28a1bc483e1ea19dcc5df1b317d27f8933d40e
theoprovost <theo.provost@epitech.eu>
2021-03-22 10:43:07 +0100

    Push credentials modified for production

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,9 +36,10 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'local',
+    key: '4872537fa5b6cea89473',
     wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS: false,
+    wssPort: 6001,
+    forceTLS: true,
     disableStats: true,
 });
