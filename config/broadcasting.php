bc329b73bf2316069f397219a60bb559f96482d2
theoprovost <theo.provost@epitech.eu>
2021-04-06 18:40:19 +0200

    Trust proxies

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => 'eu',
-                'useTLS' => true,
+                'useTLS' => false,
                 'host' => 'epitwit.tech',
                 'port' => 2053,
                 'scheme' => 'http',
